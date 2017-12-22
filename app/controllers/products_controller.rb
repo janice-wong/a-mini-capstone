42a9a4054c5030610cafd609f7c05cc94515ee67
Janice Wong <go.janicewong@gmail.com>
2017-12-22 14:04:47 -0500

    Add index and show methods in Products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,20 @@
 class ProductsController < ApplicationController
+  def index
+    products = []
+    Product.all.each do |product|
+      products << {
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        image: product.image
+      }
+    end
+
+    render json: products.as_json
+  end
+
+  def show
+    render json: Product.find(params[:id]).as_json
+  end
+
 end
--- db/migrate/20171222184926_create_products.rb
+++ db/migrate/20171222184926_create_products.rb
@@ -2,9 +2,9 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :price
+      t.decimal :price, precision: 5, scale: 2
       t.string :image
-      t.string :description
+      t.text :description
 
       t.timestamps
     end
